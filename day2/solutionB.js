3155ba9a6fd5d7aca6f47026bb76adcc68db9889
Gabe Laughlin <gabe@bistrotech.net>
2021-12-02 22:08:33 -0600

    2b

--- day2/solutionB.js
+++ day2/solutionB.js
@@ -1,22 +1,34 @@
 const { flow, reduce, split } = require('lodash/fp');
 
-const positionReducer = ([horiz, depth], command) => {
+const positionReducer = ({ horiz, depth, aim }, command) => {
   const [direction, distanceStr] = split(' ', command);
   const distance = parseInt(distanceStr);
 
   switch (direction) {
     case 'up':
-      return [horiz, depth - distance];
+      return {
+        horiz,
+        depth,
+        aim: aim - distance
+      };
     case 'down':
-      return [horiz, depth + distance];
+      return {
+        horiz,
+        depth,
+        aim: aim + distance
+      };
     case 'forward':
-      return [horiz + distance, depth];
+      return {
+        horiz: horiz + distance,
+        depth: depth + (aim * distance),
+        aim
+      };
   }
 }
 
 const solve = flow(
-  reduce(positionReducer, [0, 0]),
-  ([horiz, depth]) => horiz * depth,
+  reduce(positionReducer, { horiz: 0, depth: 0, aim: 0 }),
+  ({ horiz, depth }) => horiz * depth,
 )
 
 module.exports = solve
